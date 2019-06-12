206902d6c66568b1669d608dc72b61d039cda8bf
TMSulik <shulich@hotmail.com>
2019-06-12 15:34:53 -0700

    client build

---
+++ client/build/precache-manifest.b501f374c7e50eb8a84f0a696556d15b.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36bb41f1b810dcd3021762b22dc559e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2eb82798dd58acea7fc7",
+    "url": "/static/css/2.d58fcd6a.chunk.css"
+  },
+  {
+    "revision": "406664253fb421a7188b",
+    "url": "/static/css/main.125ba3ac.chunk.css"
+  },
+  {
+    "revision": "2eb82798dd58acea7fc7",
+    "url": "/static/js/2.cb87d786.chunk.js"
+  },
+  {
+    "revision": "406664253fb421a7188b",
+    "url": "/static/js/main.4ef0d00f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4517d2c6d637be199a71660dd9ada557",
+    "url": "/static/media/ai.4517d2c6.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f1ba3eb9c53e9d7eb495554b322f07d7",
+    "url": "/static/media/lying-on-the-couch.f1ba3eb9.jpg"
+  },
+  {
+    "revision": "615b4e70604854fd2b4851b539073ce8",
+    "url": "/static/media/persona.615b4e70.jpg"
+  },
+  {
+    "revision": "73ccc2c337519bd6dbbee8a161aef997",
+    "url": "/static/media/tat-1.73ccc2c3.jpg"
+  },
+  {
+    "revision": "04cd4a91e52b6de27d3b1b1520759b0e",
+    "url": "/static/media/tat-10.04cd4a91.jpg"
+  },
+  {
+    "revision": "89dc2bac3c5ff07a5660bba2c056fb8e",
+    "url": "/static/media/tat-11.89dc2bac.jpg"
+  },
+  {
+    "revision": "8c778dac5617a493c6ee4700f685c827",
+    "url": "/static/media/tat-12.8c778dac.jpg"
+  },
+  {
+    "revision": "e723191df733eefe0f56c16476e498b5",
+    "url": "/static/media/tat-13.e723191d.jpg"
+  },
+  {
+    "revision": "d36f3970d484be18d55da7fc8cf5238d",
+    "url": "/static/media/tat-14.d36f3970.jpg"
+  },
+  {
+    "revision": "2704896d38b087030a582d9550b84f2a",
+    "url": "/static/media/tat-15.2704896d.jpg"
+  },
+  {
+    "revision": "8fdd6c0e19ff7668a4e126fa4eff4d97",
+    "url": "/static/media/tat-16.8fdd6c0e.jpg"
+  },
+  {
+    "revision": "f67919ed875dd873298953b8c0a5e58b",
+    "url": "/static/media/tat-17.f67919ed.jpg"
+  },
+  {
+    "revision": "6070d88ceb5a6ae6238d830277987fd7",
+    "url": "/static/media/tat-18.6070d88c.jpg"
+  },
+  {
+    "revision": "0a4baf3b180a3627b966b5d6fa4bcf9d",
+    "url": "/static/media/tat-19.0a4baf3b.jpg"
+  },
+  {
+    "revision": "80e3a53a7a921531596f7eaeea8bff65",
+    "url": "/static/media/tat-2.80e3a53a.jpg"
+  },
+  {
+    "revision": "d3826ae7b05d1ed8c45d3c16528d1ea5",
+    "url": "/static/media/tat-20.d3826ae7.jpg"
+  },
+  {
+    "revision": "cf7bfb84c024f901703ff3145368abfd",
+    "url": "/static/media/tat-21.cf7bfb84.jpg"
+  },
+  {
+    "revision": "47f16233122525fac84687491ed93f85",
+    "url": "/static/media/tat-22.47f16233.jpg"
+  },
+  {
+    "revision": "8864a79684d85fc88b11d7d1a4285746",
+    "url": "/static/media/tat-23.8864a796.jpg"
+  },
+  {
+    "revision": "bd9befd2671b2fb4721251974d5b65b9",
+    "url": "/static/media/tat-24.bd9befd2.jpg"
+  },
+  {
+    "revision": "eed6f3e86ffcf7a047af05d8b57e4695",
+    "url": "/static/media/tat-25.eed6f3e8.jpg"
+  },
+  {
+    "revision": "1eb5ed5609861d8d6c537bb943e58f04",
+    "url": "/static/media/tat-26.1eb5ed56.jpg"
+  },
+  {
+    "revision": "a43da67058bbb86031b5e0013f797864",
+    "url": "/static/media/tat-3.a43da670.jpg"
+  },
+  {
+    "revision": "c394f69fa3c0204c935cb2d371dcb82c",
+    "url": "/static/media/tat-4.c394f69f.jpg"
+  },
+  {
+    "revision": "591419703a4e137c12ea69c3d664a527",
+    "url": "/static/media/tat-5.59141970.jpg"
+  },
+  {
+    "revision": "361a335ce4010968ba6d701218e66f4d",
+    "url": "/static/media/tat-6.361a335c.jpg"
+  },
+  {
+    "revision": "c7bfe62b6f236899f36ae176e00be56f",
+    "url": "/static/media/tat-7.c7bfe62b.jpg"
+  },
+  {
+    "revision": "067ffd54a6162f0c2497071085c9b2cd",
+    "url": "/static/media/tat-8.067ffd54.jpg"
+  },
+  {
+    "revision": "968f5fa7cb61434af931c58ee4ee02b3",
+    "url": "/static/media/tat-9.968f5fa7.jpg"
+  },
+  {
+    "revision": "5a4962351c161571806d542d531c0175",
+    "url": "/static/media/the_psyche.5a496235.jpg"
+  }
+]);
